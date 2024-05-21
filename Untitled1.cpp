d80c997706d5b71dd9e0b53b1e19e515805e562b
JoYoneyama <jvsyoneyama@gmail.com>
2024-05-21 14:05:01 -0300

    tela de seleçao de nivel

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -4,7 +4,7 @@
 	#include<cstdlib>
 	#define COL1 5
 
-	void Matriz1(char matriz[][COL1]){
+	void Matriz1(char matriz[][COL1]){               //matriz sendo usada de exemplo para o programa
 		
 		int i, j;
 		
@@ -27,21 +27,21 @@
 		
 		setlocale(LC_ALL, "Portuguese");
 		
-		int codigo, comandoJogar;
+		int codigo, codigoNvl, comandoJogar;
 		char matrizFacil[5][5] = {{'a', 'b', 'c', 'd', 'e'}, {'a', 'b', 'c', 'd', 'e'}, {'a', 'b', 'c', 'd', 'e'}, {'a', 'b', 'c', 'd', 'e'}, {'a', 'b', 'c', 'd', 'e'}};
 		
 		
 		do{
 			
 			printf("CAÇA-PALAVRAS");
-			printf("\n===================================\n");
+			printf("\n===================================\n");          //MENU do programa
 			printf("1. JOGAR\n2. COMO JOGAR\n3. SAIR\n\n");
 			printf("NÚMERO: ");
 			scanf("%d", &codigo);
 			
 			system("cls");
 			
-			while(codigo < 1 || codigo > 3){
+			while(codigo < 1 || codigo > 3){                        //caso n seja digitado um numero valido entre as opcoes do MENU
 				
 				printf("CAÇA-PALAVRAS");
 				printf("\n===================================\n");
@@ -53,27 +53,70 @@
 				
 			}
 			
-			switch(codigo){
+			switch(codigo){                      
 				
-				case 1:
-					Matriz1(matrizFacil);
-					scanf("%d", &comandoJogar);
+				case 1:                                      //tela para selecionar o nvl q vai ser jogado
+					printf("JOGAR - ESCOLHA UM NÍVEL");
+					printf("\n===================================\n");
+					printf("1. FÁCIL\n2. MÉDIO\n3. DÍFICIL\n4. VOLTAR\n\n");
+					printf("NÚMERO: ");
+					scanf("%d", &codigoNvl);
 					
 					system("cls");
+					
+					while(codigoNvl < 1 || codigoNvl > 4){             //caso n seja digitado um numero valido entre as opcoes
+						
+						printf("JOGAR - ESCOLHA UM NÍVEL");
+						printf("\n===================================\n");
+						printf("1. FÁCIL\n2. MÉDIO\n3. DÍFICIL\n4. VOLTAR\n\n");
+						printf("NÚMERO: ");
+						scanf("%d", &codigoNvl);
+					
+						system("cls");
+						
+					}
+					
+					switch(codigoNvl){                     
+						
+						case 1:
+							Matriz1(matrizFacil);
+							scanf("%d", &comandoJogar);  //sem codigo para jogar ainda
+							
+							system("cls");
+							break;
+							
+						case 2:
+							Matriz1(matrizFacil);           //criar matriz de nvl medio
+							scanf("%d", &comandoJogar);         //sem codigo para jogar ainda
+							
+							system("cls");
+							break;
+							
+						case 3:
+							Matriz1(matrizFacil);              // criar matriz de nvl medio
+							scanf("%d", &comandoJogar);           //sem codigo para jogar ainda
+							
+							system("cls");
+							break;
+							
+						case 4:                           //botao de VOLTAR para o menu
+							break;
+							
+					}
 					break;
 					
-				case 2:
+				case 2:                          //tela de instrucoes do jogo
 					printf("COMO JOGAR");
 					printf("\n===================================\n");
-					printf("instruçoes");
-					printf("\n");
+					printf("instruçoes");                //sem instrucoes ainda
+					printf("\n\n");
 					
-					system("pause");
+					system("pause");    //comando para o "Pressione qualquer tecla..."
 					system("cls");
 					break;
 					
-				case 3:
-					printf("SAINDO...");
+				case 3:                      //fechar o programa
+					printf("SAINDO...");       
 					
 					return 0;
 				
