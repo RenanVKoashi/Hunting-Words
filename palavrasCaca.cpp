60ec7502d4e3e0cb87c9c09dad154a9f3480b11b
Moscofian <137722753+Moscofian@users.noreply.github.com>
2024-06-07 16:15:30 -0300

    Add files via upload

--- palavrasCaca.cpp
+++ palavrasCaca.cpp
@@ -1026,6 +1026,11 @@
                 pontuacaoVetorMedioEsportes[10] = {0};
                 pontuacaoVetorDificilEsportes[12] = {0};
                 pontuacao = 0;
+                
+                x1=0;
+                y1=0;
+                x2=0;
+                y2=0;
 
                 system("pause");
                 system("cls");
